51af54803b2e8f35dfc6e5e9f81955721714bb9b
Akash <aksha66850@gmail.com>
2026-07-06 20:44:36 +0530

    fix: set dynamic base path for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/cpu-scheduling-simulator/',
+  base: process.env.VERCEL ? '/' : '/cpu-scheduling-simulator/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
